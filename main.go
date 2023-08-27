beb9ad3e1cc89fdf4020bf2a2766e622ec5635cc
tyler236712 <tygood129@gmail.com>
2023-08-26 20:38:21 -0400

    fixed page number issue

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	file, _ := os.Create("names.txt")
 
 	for i := 0; i < 200; i++ { // the stop condition should equal the number of pages
-		page := []byte(fmt.Sprintf("page%d.png", i)) // creates a byte slice of file names
+		page := []byte(fmt.Sprintf("page%d.png", i+1)) // creates a byte slice of file names
 		file.Write(append(page, 0))                  // appends the null byte to the end and writes it to the file
 	}
 
